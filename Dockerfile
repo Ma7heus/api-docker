dd163a2e294c4e5cf1559a4617221381a45c302b
Matheus Biasi <biasimatheushenrique40@gmail.com>
2025-06-05 23:24:53 -0300

    Adiciona configuração do Maven e controlador de status; implementa abertura automática do navegador na inicialização

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,17 @@
 FROM maven:3.9.9-eclipse-temurin-17 AS build
 WORKDIR /app
 
+# Defina argumentos para o token e username
+ARG GITHUB_USERNAME
+ARG GITHUB_TOKEN
+
+# Crie o settings.xml dinamicamente com as credenciais
+RUN mkdir -p /root/.m2
+
 COPY . /app/
+
+RUN echo "<settings><servers><server><id>github</id><username>${GITHUB_USERNAME}</username><password>${GITHUB_TOKEN}</password></server></servers></settings>" > /root/.m2/settings.xml
+
 RUN mvn clean package -DskipTests
 
 FROM eclipse-temurin:17-jdk
--- src/main/java/com/matheusbiasi/api_docker/ApiDockerApplication.java
+++ src/main/java/com/matheusbiasi/api_docker/ApiDockerApplication.java
@@ -1,13 +1,47 @@
 package com.matheusbiasi.api_docker;
 
+import java.awt.Desktop;
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class ApiDockerApplication {
+	
+	@Autowired
+	private Environment environment;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiDockerApplication.class, args);
 	}
 
+	@EventListener(ApplicationReadyEvent.class)
+	public void openBrowser() {
+		try {
+			String url = "http://localhost:8080";
+			if (Desktop.isDesktopSupported()) {
+				Desktop.getDesktop().browse(new URI(url));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@EventListener(ApplicationReadyEvent.class)
+	public void onApplicationReady() {
+		String port = environment.getProperty("server.port", "8080");
+		String contextPath = environment.getProperty("server.servlet.context-path", "");
+		String profileActive = environment.getProperty("spring.profiles.active", "");
+		System.out.println("***************************");
+		System.out.println("Profile: "+profileActive);
+		System.out.println("***************************");
+		System.out.println("------------------------------------------------------");
+		System.out.println("Aplicação rodando em: http://localhost:" + port + contextPath);
+		System.out.println("------------------------------------------------------");
+	}
 }
